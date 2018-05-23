5448b2fea35c10f8db99e884c9c95dbe983e50d6
zzzxj <471493996@qq.com>
2018-05-23 17:42:06 +0800

    from zhengxj

--- app01/views.py
+++ app01/views.py
@@ -17,16 +17,18 @@ username_t = 'root'
 
 def login(request):
     context = {"error":0}
+    print request.method
     if request.method == "POST":
         input_name = request.POST[u'username']
         input_pwd = request.POST[u'password']
         result = User.objects.filter(username=input_name,password=input_pwd)    
         if(len(result)<=0):
-            context[u'error'] = 1
+            context['error'] = 1
             return render(request,'login.html',context)
         else:
             global username_t
             username_t = input_name
-            return render(request,"login_succeed.html",{usernmae:username_t})
+            context ["username"] = username_t
+            return render(request,"login_succeed.html",context)
     return render(request,'login.html',context)
 
